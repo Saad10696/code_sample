20745bfc42d175a3db8b0025d21b95c105822cd7
saad <saadrazi14@gmail.com>
2024-06-15 17:17:53 +0500

    project init

---
+++ src/containers/SafeAreaContainer.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+export default SafeAreaContainer = props => {
+  const {
+    mode = 'dark',
+    backgroundColor = 'transparent',
+    style = {},
+  } = props;
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff', ...style}}>
+      <StatusBar
+        translucent={true}
+        backgroundColor={backgroundColor}
+        barStyle={mode === 'light' ? 'dark-content' : 'light-content'}
+      />
+      <SafeAreaView style={styles.container}>{props.children}</SafeAreaView>
+    </View>
+  );
+};
+
+export const Loader = () => {
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'black',
+        opacity: 0.5,
+        zIndex: 999,
+        elevation: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <ActivityIndicator color="#fff" size={'large'} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0,
+  },
+});
